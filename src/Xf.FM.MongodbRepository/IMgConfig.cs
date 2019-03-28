6b4db506651be9864f2c78e8381c3782b21962e6
xxf <2103625@qq.com>
2019-03-29 04:16:40 +0800

    1

--- src/Xf.FM.MongodbRepository/IMgConfig.cs
+++ src/Xf.FM.MongodbRepository/IMgConfig.cs
@@ -4,10 +4,11 @@ using System;
 namespace Xf.FM.MongodbRepository
 {
     /// <summary>
+    /// 单例注入
     /// 文档 ref="http://mongodb.github.io/mongo-csharp-driver/2.7/getting_started/"
     /// mongodbC#驱动源码 ref="https://github.com/mongodb/mongo-csharp-driver"
     /// </summary>
-    public interface IMgConfig:IDisposable
+    public interface IMgConfig : IDisposable
     {
         MongoClientSettings GetMgConfig();
         string GetDbName();
